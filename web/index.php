fc3e82f54da417b0c3ea803ece67d998a008ad49
nickolanack <nickblackwell82@gmail.com>
2015-08-24 15:57:19 -0700

    add content length

--- web/index.php
+++ web/index.php
@@ -28,6 +28,8 @@ if (key_exists('download', $_GET) && in_array($_GET['download'], $files)) {
     
     header('Content-Type: ' . trim($info));
     header('Content-Disposition: attachment; filename=' . addcslashes($file, '"\\'));
+    header('Content-Length: ' . filesize($dir . DS . $file));
+    
     readfile($file);
     return;
 }
